f47ad2c83ac360a4452847d10082ea270b8f59f8
Loginov-Mikhail <78172722+Loginov-Mikhail@users.noreply.github.com>
2021-06-24 10:44:41 +0300

    Добавлена автоматическая загрузка новых отчетов
    Добавлена автоматическая загрузка новых отчетов на диск

--- disclosure.py
+++ disclosure.py
@@ -1,59 +1,71 @@
-﻿#******************************************************************************
-#   Программа disclosure.py предназначена для поиска на сайте 
+﻿# *****************************************************************************
+#   Программа disclosure.py предназначена для поиска на сайте
 # www.e-disclosure.ru новых отчетов по МСФО. Исходные данные хранятся в файле
 # input.txt в формате:
 # id;value
 # где id - номер эмитента на сайте www.e-disclosure.ru;
-#     value - номер последней ссылки на странице финансовой отчетности 
+#     value - номер последней ссылки на странице финансовой отчетности
 #             эмитента.
 #   Для запуска программы необходим Python 3.0
 #   При выполнении в командной строке:
 #   python disclosure.py
-# 
 #                    Copyright (c) 2019 Логинов М.Д.
-# 
 #  Разработчик: Логинов М.Д.
-#  Модифицирован: 21 февраля 2019 г.
+#  Модифицирован: 30 марта 2020 г.
 #
-#******************************************************************************
+# ******************************************************************************
 
 # -*- coding: utf-8 -*-
-import string
 import urllib.request
+import os
 
-#******************************************************************************
+
+# ******************************************************************************
 #         Функция получения данных из Интернета и их анализа
-#******************************************************************************
-
-def read_new_data_from_internet(id):
-    url = "http://www.e-disclosure.ru/portal/files.aspx?id=0&type=4"
-    url1 = url.replace('0',id)
+# ******************************************************************************
+def read_new_data(id):
+    url = 'http://www.e-disclosure.ru/portal/files.aspx?id=0&type=4&attempt=1'
+    url1 = url.replace('0', id)
     response = urllib.request.urlopen(url1).read()
     f = str(response).split('\\r')
     # Следующая строка необходима для отладки offline
-    #f=open('temp.html','r',encoding='utf-8')
+    # f=open('temp.html','r',encoding='utf-8')
     for line in f:
         if line.count('Fileid') > 0:
-            #print(line)
+            # print(line)
             str1 = line.split('Fileid=')
             str2 = str1[1].split('"')
             str3 = str2[0]
             break
-    #f.close()
+    # f.close()
     return str3
 
-#******************************************************************************
-#                       Текст основной программы
-#******************************************************************************
 
-print("START")
-f = open('input.txt','r')
+# ******************************************************************************
+#                  Функция сохранения отчета на диск
+# ******************************************************************************
+def save_report(id):
+    url = 'http://www.e-disclosure.ru/portal/FileLoad.ashx?Fileid='
+    tempFile, headers = urllib.request.urlretrieve(url+id, "tmp")
+    for line in headers.values():
+        if line.count('filename') > 0:
+            print(line.split('=')[1].replace('"', ''))
+            os.rename('tmp', line.split('=')[1].replace('"', ''))
+    return 0
+
+
+# ******************************************************************************
+#                       Текст основной программы
+# ******************************************************************************
+print('START')
+f = open('input.txt', 'r')
 for line in f:
     str1 = line.split(";")
     id = str1[0]
     value = str1[1]
-    newData = read_new_data_from_internet(id)
+    newData = read_new_data(id)
     if value[:-1] != newData:
         print(id)
+        save_report(newData)
 f.close()
-print("STOP")
+print('STOP')
