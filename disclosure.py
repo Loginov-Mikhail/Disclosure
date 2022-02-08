9e04b7474cc5699990f8c2dd058cdfc9659cd7e9
Mikhail Loginov <loginovmd@rambler.ru>
2022-02-08 21:24:51 +0300

    Изменен формат входных данных: вместо файла input.txt используется лист "Годовой отчет 2021" в таблице Excel input.xls

--- disclosure.py
+++ disclosure.py
@@ -1,29 +1,32 @@
 ﻿# *****************************************************************************
 #   Программа disclosure.py предназначена для поиска на сайте
 # www.e-disclosure.ru новых отчетов по МСФО. Исходные данные хранятся в файле
-# input.txt в формате:
-# id;value
-# где id - номер эмитента на сайте www.e-disclosure.ru;
-#     value - номер последней ссылки на странице финансовой отчетности
-#             эмитента.
-#   Для запуска программы необходим Python 3.0 и библиотеки urllib3, certifi
+# input.xls на листе "Годовой отчет 2021". Необходимыми данными являются:
+# - тикер;
+# - прибыль;
+# - номер эмитента на сайте www.e-disclosure.ru;
+# - тип публикуемой отчетности на сайте www.e-disclosure.ru;
+# - номер последней ссылки на странице финансовой отчетности эмитента.
+#   Для запуска программы необходим Python 3.0 и библиотеки urllib3, certifi,
+# xlrd, pandas.
 #   При выполнении в командной строке:
 #   python disclosure.py
-#                    Copyright (c) 2019 Логинов М.Д.
+#                    Copyright (c) 2019 - 2022 Логинов М.Д.
 #  Разработчик: Логинов М.Д.
-#  Модифицирован: 24 июня 2021 г.
+#  Модифицирован: 8 февраля 2022 г.
 #
-# ******************************************************************************
+# *****************************************************************************
 
 # -*- coding: utf-8 -*-
 import os
 import urllib3
 import certifi
+import pandas as pd
 
 
-# ******************************************************************************
+# *****************************************************************************
 #         Функция получения куки от сервера
-# ******************************************************************************
+# *****************************************************************************
 def get_cookie():
     url = 'https://www.e-disclosure.ru/portal/files.aspx?id=9&type=4'
     http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',
@@ -36,12 +39,13 @@ def get_cookie():
     return resp.getheaders()['Set-Cookie']
 
 
-# ******************************************************************************
+# *****************************************************************************
 #         Функция получения данных из Интернета и их анализа
-# ******************************************************************************
-def read_new_data(id, cookies):
-    url = 'https://www.e-disclosure.ru/portal/files.aspx?id=0&type=4'
-    url1 = url.replace('0', id)
+# *****************************************************************************
+def read_new_data(id, typeId, cookies):
+    url = 'https://www.e-disclosure.ru/portal/files.aspx?id=0&type='\
+          + str(typeId)
+    url1 = url.replace('0', str(id))
     http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',
                                ca_certs=certifi.where(),
                                timeout=urllib3.Timeout(connect=1.0, read=2.0))
@@ -60,14 +64,14 @@ def read_new_data(id, cookies):
             str3 = str2[0]
             break
     # f.close()
-    return str3
+    return int(str3)
 
 
-# ******************************************************************************
+# *****************************************************************************
 #                  Функция сохранения отчета на диск
-# ******************************************************************************
+# *****************************************************************************
 def save_report(id, cookies):
-    url = 'https://www.e-disclosure.ru/portal/FileLoad.ashx?Fileid=' + id
+    url = 'https://www.e-disclosure.ru/portal/FileLoad.ashx?Fileid=' + str(id)
     http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',
                                ca_certs=certifi.where(),
                                timeout=urllib3.Timeout(connect=1.0, read=2.0))
@@ -83,19 +87,36 @@ def save_report(id, cookies):
     return 0
 
 
-# ******************************************************************************
+# *****************************************************************************
+#               Функция чтения исходных данных из файла Excel
+# *****************************************************************************
+def read_input():
+    data = pd.read_excel('input.xls', 'Годовой отчет 2021',
+                         usecols=[1, 4, 15, 16, 17])
+    data.set_axis(['ticker', 'profit', 'id', 'type', 'last'], axis=1,
+                  inplace=True)
+    data['profit'] = data['profit'].fillna(0)
+    data = data[data['profit'] == 0]
+    del data['profit']
+    data['id'] = data['id'].fillna(0)
+    data = data[data['id'] > 0]
+    data['id'] = data['id'].astype('uint32')
+    data['type'] = data['type'].fillna(4)
+    data['type'] = data['type'].astype('uint8')
+    data['last'] = data['last'].astype('uint32')
+    data = data.reset_index(drop=True)
+    return data
+
+
+# *****************************************************************************
 #                       Текст основной программы
-# ******************************************************************************
+# *****************************************************************************
+data = read_input()
 print('START')
 siteCookie = get_cookie()
-f = open('input.txt', 'r')
-for line in f:
-    str1 = line.split(";")
-    id = str1[0]
-    value = str1[1]
-    newData = read_new_data(id, siteCookie)
-    if value[:-1] != newData:
-        print(id)
+for i in range(len(data)):
+    newData = read_new_data(data.loc[i, 'id'], data.loc[i, 'type'], siteCookie)
+    if data.loc[i, 'last'] != newData:
+        print(data.loc[i, 'ticker'] + ';' + str(newData))
         save_report(newData, siteCookie)
-f.close()
 print('STOP')
